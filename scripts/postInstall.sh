32f860e0eb00b8d8caf45e69d3a3d7d9c5df7426
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-04-11 14:57:14 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,6 +3,6 @@ set -o allexport; source .env; set +o allexport;
 
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
-sleep 60s;
+sleep 120s;
 
 docker-compose -p mailu exec admin flask mailu admin admin ${DOMAIN} ${ADMIN_PASSWORD}
