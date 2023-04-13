528bacf9b387e9311b7ec31fa11d2ea41f11d279
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-04-13 10:09:06 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -8,8 +8,3 @@ sed -i "s~\SECRET_KEY=~SECRET_KEY=${SECRET_KEY}~g" ./mailu.env
 sed -i "s~\API_TOKEN=~API_TOKEN=${API_TOKEN}~g" ./mailu.env
 sed -i "s~\${DOMAIN}~${DOMAIN}~g" ./mailu.env
 
-# apt install jq -y
-
-# mkdir -p ./data
-# chown -R 1000:1000 ./data
-
